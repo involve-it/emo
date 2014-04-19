1c0de0a4abd1fee82377e3025e516acc734b8209
arutune <arutune@gmail.com>
2014-04-18 23:06:50 -0700

    Project updates for running on any machine.

--- server/node.js
+++ server/node.js
@@ -26,9 +26,7 @@
   var express = require('express'),
     http = require('http');
   var app = express();
-  app.configure(function(){
-    //app.use(express.static(__dirname));
-  });
+
   app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
